0b9337c3c8508a0b7aeef9c722933e6f203cc7a7
Daniel G <danoguerrerok@gmail.com>
2021-09-30 13:04:40 -0500

    cerrando_proyecto

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -21,7 +21,10 @@ const Home = ({ myList, trends, originals }) => {
                     <Categories key={category} title={category}>
                         <Carousel>
                             {lists[i].map((item) => (
-                                <CarouselItem key={item.id} {...item} />
+                                <CarouselItem
+                                key={item.id}
+                                {...item}
+                                />
                             ))}
                         </Carousel>
                     </Categories>
